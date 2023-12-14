c500721eaad75044a4a2df4fe9f87b6c9f0dbaed
ritika-saha <ritika882001@gmail.com>
2023-12-15 01:40:09 +0530

    initial setup

---
+++ src/main/java/com/census/Main.java
@@ -0,0 +1,7 @@
+package com.census;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("================================= STATE CENSUS ===============================");
+    }
+}
